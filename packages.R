2dc9c2c19b0d11d21b3a7caa90b0d07d49250297
Clementine Cottineau <clementine.cottineau@iscpif.fr>
2016-02-03 11:39:14 +0000

    add packages file

---
+++ packages.R
@@ -0,0 +1,13 @@
+my_packages = c("ggplot2", "RColorBrewer", "plyr", "shiny")
+
+###########################################################
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
