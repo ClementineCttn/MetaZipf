df724618acc32fe91ce3e63acacfcf1b9a951969
Clementine Cottineau <clementine.cottineau@iscpif.fr>
2019-08-22 15:38:51 +0200

    add disciplines in data + viz

--- MetaMetaZipf.R
+++ MetaMetaZipf.R
@@ -1,36 +1,57 @@
 setwd("~/GitHub/MetaZipf")
 library(igraph)
+library(scales)
+library(wesanderson)
 
-cites <- read.csv2("data/zipf_cites.csv", sep=',')
+cites <- read.csv2("data/zipf_cites.csv", sep=';')
 head(cites)
 
 cites[is.na(cites)] <- 0
 dim(cites)
 
-cites_within <- cites[1:66,c(1,5:70)]
+cites_within <- cites[1:66,c(1,6:71)]
 tail(cites_within)
 rownames(cites_within) <- cites_within$REFID
 cites_within$REFID <- NULL
 cmat <- t(as.matrix(cites_within))
 dim(cmat)
 
-node.size <- colSums(cmat)
+node.size <- colSums(cmat) +1
 
 
 cite_year <- cites[1:66,c('REFID', 'YEAR')]
-rownames(cite_year) <- cite_year$REFID
+ids <- cite_year$REFID
+rownames(cite_year) <- ids
 cite_year$REFID <- NULL
 node.year <- cite_year$YEAR - 1974
+
+cite_disc <- cites[1:66,c('REFID', 'DISCIPLINE')]
+rownames(cite_disc) <- ids
+cite_disc$REFID <- NULL
+node.disc <- as.character(cite_disc$DISCIPLINE)
+
+pal.vert <- wes_palette("Zissou1", 4, type = "continuous")
+node.disc[node.disc == "ECO"] <- pal.vert[1]
+node.disc[node.disc == "REG"] <- pal.vert[2]
+node.disc[node.disc == "GEO"] <- pal.vert[3]
+node.disc[node.disc == "PHY"] <- pal.vert[4]
+
+
 
 g <- graph_from_adjacency_matrix(cmat, mode = "directed", diag = F)
 
 layout <- layout_nicely(g,2)
 g$layout <- layout
 fine = 500 # this will adjust the resolving power.
-pal = colorRampPalette(c('black','white'))
-graphCol = pal(fine)[as.numeric(cut(node.year,breaks = fine))]
+pal.lab = colorRampPalette(c('grey30','black'))
+graphCol.lab = pal.lab(fine)[as.numeric(cut(node.year,breaks = fine))]
+
+par(mar = c(0.5, 0.3, 0.5, 0.5))
 
 plot(g, vertex.size=node.size*0.5,
      vertex.label.dist=0.5, 
-     vertex.color=graphCol, edge.arrow.size=0.2,
-     vertex.label.cex = 0.5)
+     vertex.color= alpha("grey",0.6), edge.color = "black", 
+     vertex.label.cex = 0.7,  vertex.label.color = node.disc,
+     vertex.label.font = 2,  
+     edge.arrow.size=0.2)
+
